9b1b5c8ab08bcb3fd4bd73292baedb731e85a453
raul reymundi <kinskispiral@gmail.com>
2022-05-21 22:35:50 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.getElementById("create-task-form").addEventListener("submit", (e) => {
+    e.preventDefault()
+    buildTask(e.target.new-task-description.value)
+    form.reset()
+  });
 });
+
+function buildTask(task){
+  let p = document.createElement("p")
+  let btn = document.createElement("button")
+  btn.addEventListener("click", handleDelete)
+  btn.textContent = "x"
+  p.textContent = task
+  p.appendChild(btn)
+  document.querySelector("new-task-description").appendChild(p)
+};
+
+function handleDelete(e){
+  e.target.parentNode.remove()
+}
